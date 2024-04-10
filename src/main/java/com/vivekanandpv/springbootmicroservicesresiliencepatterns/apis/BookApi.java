fc4cff848212538f7fdb4f9460b207fe003262ec
Vivekanand P V <vivekanandpv@outlook.com>
2024-04-10 16:36:25 +0530

    retry pattern implemented

--- src/main/java/com/vivekanandpv/springbootmicroservicesresiliencepatterns/apis/BookApi.java
+++ src/main/java/com/vivekanandpv/springbootmicroservicesresiliencepatterns/apis/BookApi.java
@@ -3,6 +3,10 @@ package com.vivekanandpv.springbootmicroservicesresiliencepatterns.apis;
 import com.vivekanandpv.springbootmicroservicesresiliencepatterns.models.Book;
 import com.vivekanandpv.springbootmicroservicesresiliencepatterns.services.BookService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.RetryRegistry;
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,13 +19,23 @@ import java.util.Map;
 @RequestMapping("api/v1/books")
 public class BookApi {
     private final RestTemplate restTemplate;
+    private final Logger logger;
 
-    public BookApi(RestTemplate restTemplate) {
+    public BookApi(RestTemplate restTemplate, RetryRegistry retryRegistry) {
         this.restTemplate = restTemplate;
+        logger = LoggerFactory.getLogger(this.getClass());
+
+        //  logging the retry events
+        retryRegistry.getAllRetries().forEach(r -> {
+            r.getEventPublisher()
+                    .onRetry(e -> logger.info(String.format("Retry: %s", e)))
+                    .onError(e -> logger.info(String.format("Error: %s", e)))
+                    .onSuccess(e -> logger.info(String.format("Success: %s", e)));
+        });
     }
 
 
-    @CircuitBreaker(name = "downstream", fallbackMethod = "getFallback")
+    @Retry(name = "downstream", fallbackMethod = "getFallback")
     @GetMapping
     public ResponseEntity<Map<String, String>> get() {
         ResponseEntity<Book> responseEntity = restTemplate.getForEntity("/api/v1/books", Book.class);
