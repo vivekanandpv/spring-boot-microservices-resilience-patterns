697e207d18fe5d7f2ec2416200bf5d1be8ba87f2
Vivekanand P V <vivekanandpv@outlook.com>
2024-04-10 16:40:36 +0530

    logging added

--- src/main/java/com/vivekanandpv/springbootmicroservicesresiliencepatterns/apis/BookApi.java
+++ src/main/java/com/vivekanandpv/springbootmicroservicesresiliencepatterns/apis/BookApi.java
@@ -2,7 +2,10 @@ package com.vivekanandpv.springbootmicroservicesresiliencepatterns.apis;
 
 import com.vivekanandpv.springbootmicroservicesresiliencepatterns.models.Book;
 import com.vivekanandpv.springbootmicroservicesresiliencepatterns.services.BookService;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,9 +18,20 @@ import java.util.Map;
 @RequestMapping("api/v1/books")
 public class BookApi {
     private final RestTemplate restTemplate;
+    private final Logger logger;
 
-    public BookApi(RestTemplate restTemplate) {
+    public BookApi(RestTemplate restTemplate, CircuitBreakerRegistry circuitBreakerRegistry) {
         this.restTemplate = restTemplate;
+        logger = LoggerFactory.getLogger(this.getClass());
+
+        circuitBreakerRegistry.getAllCircuitBreakers().forEach(c -> {
+            c.getEventPublisher()
+                    .onSuccess(e -> logger.info(String.format("Success: %s", e)))
+                    .onError(e -> logger.info(String.format("Error: %s", e)))
+                    .onFailureRateExceeded(e -> logger.info(String.format("Failure Rate Exceeded: %s", e)))
+                    .onStateTransition(e -> logger.info(String.format("State Transition: %s", e)))
+                    .onReset(e -> logger.info(String.format("Reset: %s", e)));
+        });
     }
 
 
