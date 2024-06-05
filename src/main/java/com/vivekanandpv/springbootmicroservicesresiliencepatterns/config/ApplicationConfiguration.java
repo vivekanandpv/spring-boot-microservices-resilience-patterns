9ff8e3db665c080f5accdd109e999a9294c3f537
Vivekanand P V <vivekanandpv@outlook.com>
2024-06-05 10:32:32 +0530

    done

--- src/main/java/com/vivekanandpv/springbootmicroservicesresiliencepatterns/config/ApplicationConfiguration.java
+++ src/main/java/com/vivekanandpv/springbootmicroservicesresiliencepatterns/config/ApplicationConfiguration.java
@@ -8,14 +8,14 @@ import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class ApplicationConfiguration {
-    private final String downstreamUrl;
+    private final String upstreamUrl;
 
-    public ApplicationConfiguration(@Value("${app.downstream.url}")String downstreamUrl) {
-        this.downstreamUrl = downstreamUrl;
+    public ApplicationConfiguration(@Value("${app.upstream.url}")String upstreamUrl) {
+        this.upstreamUrl = upstreamUrl;
     }
 
     @Bean
     public RestTemplate getRestTemplate() {
-        return new RestTemplateBuilder().rootUri(downstreamUrl).build();
+        return new RestTemplateBuilder().rootUri(upstreamUrl).build();
     }
 }
